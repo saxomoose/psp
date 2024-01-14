8ff81df55a27353de677351e0114ec033617e101
saxomoose <mathieu.tulpinck@gmail.com>
2024-01-14 14:57:07 +0000

    Save

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
-console.log("test")
+import * as fs from "node:fs";
+import * as csv from "csv";
+
+const filePath = "enums.csv";
+
+const processFile = fs
+    .createReadStream(filePath)
+    .pipe(csv.parse({ delimiter: ",", from_line: 2 }))
+    .on("data", function (row) {
+        console.log(row);
+    });
+
